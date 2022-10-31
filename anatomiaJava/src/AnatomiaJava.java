86ddfe705f2e07aec25c1f2b61b3882738c7e37f
Wergiley <werslley.robson@gmail.com>
2022-10-31 13:29:19 -0300

    att

---
+++ anatomiaJava/src/AnatomiaJava.java
@@ -0,0 +1,12 @@
+public class AnatomiaJava {
+    public static void main(String[] args) {
+        String primeiroNome = "Gustavo";
+        String segundoNome = "Henrique";
+
+        String nomeCompleto = nomeCompleto;
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return concat();
+    }
+}
