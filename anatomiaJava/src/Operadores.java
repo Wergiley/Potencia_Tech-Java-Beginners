c35998335d7be64f6759c0d74c670fcd0053718c
Wergiley <werslley.robson@gmail.com>
2022-11-02 12:31:21 -0300

    att

--- anatomiaJava/src/Operadores.java
+++ anatomiaJava/src/Operadores.java
@@ -8,6 +8,34 @@ public class Operadores {
             System.out.println(concatenacao);
             concatenacao = 1+"1"+1;
             System.out.println(concatenacao);
+
+            /*Operador Unário */
+            int numero = -5;
+            numero = numero * -1;
+
+            System.out.println(numero);
+
+            /*Incremento */
+            int numero2 = 10;
+            numero2 = ++ numero2;
+
+            System.out.println(numero2);
+
+            /*Decremento */
+            numero2 = -- numero2;
+            System.out.println(numero2);
+
+            /*Ternario */
+            int a, b;
+            a = 5;
+            b = 6;
+            String resultado;
+            if(a == b) {
+                resultado = "verdadeiro";
+            } else {
+                resultado = "Falso";
+            }
+            System.out.println(resultado);
         }    
 }
 
