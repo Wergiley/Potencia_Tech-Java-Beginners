458c0e51eb83b2226c11cc6075e4427da6be7b50
Wergiley <werslley.robson@gmail.com>
2022-11-01 21:59:35 -0300

    att

--- anatomiaJava/src/TiposVariaveis.java
+++ anatomiaJava/src/TiposVariaveis.java
@@ -1,7 +1,17 @@
 public class TiposVariaveis {
 
     public static void main(String[] args) throws Exception {
+
+        /*Tipos Variaveis */
         double sal_Minimo = 2500;
+        short numCurto = 1;
+        int numNormal = numCurto;
+        short numCurto2 = (short) numNormal;
+        String nome = "Gustavo";
+
+        /*Constantes */
+
+
         System.out.println(sal_Minimo);
     }
 }
