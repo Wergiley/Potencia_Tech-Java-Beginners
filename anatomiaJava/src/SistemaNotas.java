0ce80794bbf7dc38c0e6fe0f78f8d3988e00fa16
Wergiley <werslley.robson@gmail.com>
2022-11-06 13:11:45 -0300

    att

--- anatomiaJava/src/SistemaNotas.java
+++ anatomiaJava/src/SistemaNotas.java
@@ -12,33 +12,56 @@ public class SistemaNotas {
         int media;
 
         /*Entrada de dados */
-
+        System.out.println("SISTEMA DE NOTAS POR MATÉRIA");
+        
         System.out.println("Digite seu nome: ");
         nome = scan.next();
-
+        
         System.out.println("Digite sua primeira nota: ");
         nota1 = scan.nextInt();
+
+        while(nota1<0 | nota1>10){
+            System.out.println("Nota invalida! tente novamente");
+            nota1 = scan.nextInt();
+        }
 
         System.out.println("Digite sua segunda nota: ");
         nota2 = scan.nextInt();
 
+        while(nota2<0 | nota2>10){
+            System.out.println("Nota invalida! tente novamente");
+            nota2 = scan.nextInt();
+        }
+
         System.out.println("Digite sua terceira nota: ");
         nota3 = scan.nextInt();
+
+        while(nota3<0 | nota3>10){
+            System.out.println("Nota invalida! tente novamente");
+            nota3 = scan.nextInt();
+        }
 
         System.out.println("Digite sua quarta nota: ");
         nota4 = scan.nextInt();
 
+        while(nota4<0 | nota4>10){
+            System.out.println("Nota invalida! tente novamente");
+            nota4 = scan.nextInt();
+        }
+
         media = (nota1 + nota2 + nota3 + nota4) / 4;
 
         /*Saída de dados */
-        System.out.println("SISTEMA DE BOLETIM DE NOTA POR MATÉRIA");
+    
         if( media >= 6) {
             System.out.println("Parabéns, "+nome+" você foi aprovado! Com a média de "+media);
         }
-        else if(media >= 5 && media < 6 ) {
+        else if(media >= 5 | media < 6 ) {
             System.out.println("Você está em fase de recuperação, boa sorte! Sua média foi "+media);
         } else {
             System.out.println("Infelizmente, você foi reprovado. Sua média foi "+media);
         }
+
     }
+
 }   
