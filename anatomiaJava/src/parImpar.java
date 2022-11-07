fd39d25a8e51763375a63ecff6dad763d48fcdf7
Wergiley <werslley.robson@gmail.com>
2022-11-06 23:05:56 -0300

    att

--- anatomiaJava/src/parImpar.java
+++ anatomiaJava/src/parImpar.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class parImpar {
     /*em pratica... */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quanNumeros, numero;
+        int quantPar = 0, quantImpar = 0;
+
+
+        System.out.println("Quantidade de números: ");
+        quanNumeros = scan.nextInt();
+
+        int cont = 0;
+        do {
+            System.out.println("Numero: ");
+            numero = scan.nextInt();
+            if(numero % 2 == 0) quantPar++;
+            else{
+                quantImpar++;
+            }
+            cont++;
+        } while(cont < quanNumeros);
+        System.out.println("Quantidade Par: "+ quantPar);
+        System.out.println("Quantidade Impar: "+quantImpar);
+    }
 }
