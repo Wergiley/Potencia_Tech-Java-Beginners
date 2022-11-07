cd2f92e3840c336e7248f310a90c98212209bb4c
Wergiley <werslley.robson@gmail.com>
2022-11-06 23:30:09 -0300

    att

--- anatomiaJava/src/fatorial.java
+++ anatomiaJava/src/fatorial.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class fatorial {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        int fatorial;
+
+        System.out.println("Fatorial de!: ");
+        fatorial = scan.nextInt();
+
+        int mult = 1;
+
+        for(int i = fatorial; i >= 1; i--){
+           mult = mult * i;
+        }
+        System.out.println(mult);
+
     }
 }
