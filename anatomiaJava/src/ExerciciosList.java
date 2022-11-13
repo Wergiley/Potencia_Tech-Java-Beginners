4405b5112f192b0d3a4ea763c96b3667f567f478
Wergiley <werslley.robson@gmail.com>
2022-11-13 12:04:21 -0300

    att

---
+++ anatomiaJava/src/ExerciciosList.java
@@ -0,0 +1,24 @@
+public class ExerciciosList {
+ 
+    public static void main(String[] args) {
+    // Ex 01 Faça um programa que receba a temperatura média dos 6 primeiros meses do ano e armazene-as em uma lista
+    // Após isto, calcule a média semestral das temperaturas e mostre todas as temperaturas acima desta média,
+    // E em que mês elas ocorreram, mostrar o mês por extenso: 1 - janeiro, 2 fevereiro e etc...
+
+    // Código...
+
+    // Ex 02 Utilizando lista, faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
+    // "Você conhecia a vitima?"
+    // "Esteve no local do crime?"
+    // "Mora perto da vitima?"
+    // "Devia para a vitima?"
+    // "Já trabalhou com a vitima?"
+
+    /*  Caso seja respondido como positivo 2 das 5 perguntas, ela será suspeita, 
+        entre 3 e 4, cúmplice, 5/5, assassino(a), caso nenhuma, inocente. */
+
+    // Código...
+
+    
+    }
+}
