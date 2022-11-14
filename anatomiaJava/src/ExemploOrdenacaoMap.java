bf26cd01aeb75cf5d38691cba09fb370db7df1cf
Wergiley <werslley.robson@gmail.com>
2022-11-14 19:35:28 -0300

    att

---
+++ anatomiaJava/src/ExemploOrdenacaoMap.java
@@ -0,0 +1,62 @@
+public class ExemploOrdenacaoMap {
+    //Crie um dicionario e ordene este dicionario:
+    // Exibindo Nome = Autor, Nome = Livro.
+    /*
+     * Autor = Hawking - Livro = Uma Breve Historia do Tempo, Paginas = 256.
+     * Autor = Duhiga - Livro = O poder do Hábito, Paginas = 408.
+     * Autor = Harari - Livro = 21 Lições para o seculo 21, Paginas = 432.
+     */
+
+    // Código a começar...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+     /* EXERCÍCIOS PROPOSTOS
+        Ex 01 - Dada a população estimada de alguns estados do NE brasileiro, faça:
+        Estado PE = População 9.616.621
+        Estado AL = População 3.251.543
+        Estado CE = População 9.187.103
+        Estado RN = População 3.534.265
+
+        - Crie um dicionário e relacione os estados e suas populações;
+        - Substitua a população do RN por 3.534.165
+        - Confira se a PB está no dicionario, caso não, adicione: PB - 4.039.277
+        - Exiba a população de PE.
+        - Exiba todos os estados e população na ordem que foram informados. 
+        - Exiba os estados e suas população em ordem alfabetica.
+        - Exiba o estado com menor população e sua quantidade.
+        - Exiba o estado com maior população e sua quantidade. 
+        - Exiba a soma da população desses estados.
+        - Exiba a média da população.
+        - Remova os estados com a população menor que 4.000.000.
+        - Apague o dicionario de estados.
+        - Confira se o dicionario está vazio.
+      */
+
+      /*
+        Ex 02 Façã um programa que simule um laçamento de dados.
+        Lance o dado 100 vezes e armazene.
+        Depois, mostre quantas vezes cada valor foi inserido.
+
+        Exemplo: 
+        1 - key(Obejto)+value(Set Objetos)
+        2 - key(Obejto)+value(Objeto)
+      */
+}
