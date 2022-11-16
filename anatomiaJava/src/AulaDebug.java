788fec2639f7d2dabe702c4729a1c9a43927c3a7
Wergiley <werslley.robson@gmail.com>
2022-11-16 12:40:52 -0300

    att

--- anatomiaJava/src/AulaDebug.java
+++ anatomiaJava/src/AulaDebug.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class AulaDebug {
     // Formas de identificar erros de lógica no código com: Teste de Mesa e Debbuging da IDE.
     // Onde está o erro lógico deste código? 
+    // O erro é o tipo de variável, pois está sendo retornado um inteiro, não um numero real.
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String[] alunos = {"Camila", "João", "Pedro", "Paula"};
@@ -11,11 +12,12 @@ public class AulaDebug {
 
         System.out.printf("Média da turma: %d ", media);
     }
-    public static int  calcMedia(String[] alunos, Scanner scanner) {
+
+    public static int calcMedia(String[] alunos, Scanner scanner) {
         int soma = 0;
         for(String aluno : alunos) {
             System.out.printf("Nota do aluno %s: ", aluno);
-            int nota = scanner.nextInt();
+            Double nota = scanner.nextDouble();
             soma += nota;    
         }
         return soma / alunos.length;
