cd9e3067224afb447d0a49187363f7bc2c554cf9
Wergiley <werslley.robson@gmail.com>
2022-11-17 13:16:26 -0300

    att

--- desafiodioPoo/bootcamp/src/BootCamp.java
+++ desafiodioPoo/bootcamp/src/BootCamp.java
@@ -7,8 +7,10 @@ import java.util.Objects;
 import java.util.Set;
 
 public class BootCamp {
+    
     private String nome;
     private String descricao;
+
     private final LocalDate dataInicial = LocalDate.now();
     private final LocalDate dataFinal = dataInicial.plusDays(45);
 
