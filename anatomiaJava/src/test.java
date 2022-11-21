490c82aea5a30dd9d5531392212f48e81426bfc9
Wergiley <werslley.robson@gmail.com>
2022-11-21 12:01:01 -0300

    att

--- anatomiaJava/src/test.java
+++ anatomiaJava/src/test.java
@@ -1,4 +1,4 @@
-import java.util.*;
+
 import java.util.Scanner;
 
 class test {
@@ -17,17 +17,20 @@ class test {
     	  {
     		  nums[i] = input.nextInt();
     	  }
-    
+
+        // 
         int j = 0;
 
         for (i = 0; i < nums.length; i++) {
             if (nums[i] % 2 == 0) {
                 nums[i] = nums[i] ^ nums[j] ^ (nums[j] = nums[i]);
+
             } else if(nums[i] % 2 == 0) {
                 //  Código Aqui 
             }
         }
         System.out.println("Dados de saída: ");
+        // Atribuição de valores do vetor em entry /*.
         for (var entry : nums) {
             System.out.println(entry);
         }
